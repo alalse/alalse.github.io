c3a4702973e2d0068a60b7ceab2049ff1e893ba7
alalse <albin.alvarsson@gmail.com>
2021-04-14 13:42:06 +0200

    Initial deployment

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/env sh
-
 # abort on errors
 set -e
-
 # build
 npm run build
-
 # navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-# git init
-# git add -A
-# git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:alalse/alalse.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+# deploying to https://<USERNAME>.github.io
+git push -f git@github.com.alalse/alalse.github.io.git master
 cd -
